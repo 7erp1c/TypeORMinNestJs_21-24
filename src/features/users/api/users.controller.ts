a1e9f0d6d5f1d8d5312e8b0505bbd42b85c24345
7erp1c <ul_tray@bk.ru>
2024-07-16 00:01:05 +0300

    add method create get delete user

--- src/features/users/api/users.controller.ts
+++ src/features/users/api/users.controller.ts
@@ -41,7 +41,7 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   async getAll(@Query() query: QueryUsersRequestType) {
     const { sortData, searchData } = createQuery(query);
-    return await this.userQueryRepositorySql.getAllUsers(sortData, searchData);
+    return await this.usersQuery.getAllUsers(sortData, searchData);
   }
 
   @Post()
